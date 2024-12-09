5970943cad7598781ac3194d41fab8b832ed886c
Pushp sopori <91676882+roar404@users.noreply.github.com>
2024-12-09 17:07:33 +0530

    allure

--- playwright.config.js
+++ playwright.config.js
@@ -1,56 +1,52 @@
-// playwright.config.js
-const { devices } = require('@playwright/test');
+const { defineConfig } = require('@playwright/test');
 
-module.exports = {
-    // Global settings
+module.exports = defineConfig({
+    reporter: [
+        ['list'],
+        ['allure-playwright'],
+        ['json', { outputFile: 'test-results/results.json' }],
+        ['html', { open: 'never' }],
+    ],
     use: {
-        headless: false, // Set to true if you want to run tests in headless mode
-        viewport: { width: 1280, height: 720 },
+        headless: false,
+        viewport: null,
         ignoreHTTPSErrors: true,
-        video: 'retain-on-failure', // Record video only when a test fails
-        screenshot: 'only-on-failure', // Take screenshot only when a test fails
-        baseURL: 'https://playwright.dev', // Base URL for your tests
-        trace: 'on-first-retry', // Collect trace on first retry
+        video: 'retain-on-failure',
+        screenshot: 'only-on-failure',
+        baseURL: 'https://playwright.dev',
+        trace: 'on-first-retry',
     },
-
-    // Configure projects for different browsers
     projects: [
         {
             name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
+            use: {
+                ...devices['Desktop Chrome'],
+                launchOptions: {
+                    args: ['--kiosk'],
+                },
+            },
         },
         {
             name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
+            use: {
+                ...devices['Desktop Firefox'],
+                launchOptions: {
+                    args: ['--kiosk'],
+                },
+            },
         },
         {
             name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
-        },
-        {
-            name: 'Mobile Chrome',
-            use: { ...devices['Pixel 5'] },
-        },
-        {
-            name: 'Mobile Safari',
-            use: { ...devices['iPhone 12'] },
+            use: {
+                ...devices['Desktop Safari'],
+                launchOptions: {
+                    args: ['--kiosk'],
+                },
+            },
         },
     ],
-
-    // Configure retries
     retries: 2,
-
-    // Configure reporters
-    reporter: [
-        ['list'], // Default reporter
-        ['json', { outputFile: 'test-results/results.json' }], // JSON reporter
-        ['html', { open: 'never' }], // HTML reporter
-    ],
-
-    // Configure test timeout
-    timeout: 60000, // Set timeout for each test to 60 seconds
-
-    // Configure global setup and teardown
+    timeout: 60000,
     globalSetup: require.resolve('./global-setup'),
     globalTeardown: require.resolve('./global-teardown'),
-};
+});
